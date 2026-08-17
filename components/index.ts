3e5fa3f60611b9c51b2522f00a28f2cf35cdfd51
vardhineeditharak <vardhineedi.tharak@gmail.com>
2026-08-17 22:23:33 +0530

    refactor(arch): add barrel exports and clean up project structure

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+export { AlarmAlertModal } from './AlarmAlertModal';
+export { AlarmOptionsModal } from './AlarmOptionsModal';
+export { PermissionModal } from './PermissionModal';
+export { RadiusSliderWidget } from './RadiusSliderWidget';
+export { SearchBar } from './SearchBar';
+export { WakeMapView, type WakeMapRef, type WakeMapViewProps } from './WakeMapView';
---
+++ services/index.ts
@@ -0,0 +1,13 @@
+export { alarmSoundService, type AlarmTone, type VibrationStyle } from './alarmSoundService';
+export {
+  photonSearch,
+  calculateRoutePath,
+  type GeoCoordinate,
+  type SearchResultItem,
+  type RouteResponse,
+} from './apiService';
+export {
+  WAKEPOINT_PROXIMITY_TASK,
+  setupNotificationChannel,
+  type ProximityTaskData,
+} from './backgroundTask';
